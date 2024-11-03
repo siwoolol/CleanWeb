1be97d1134d5acccdb43670811cd3200e0a31eb7
siwoo <jamieyun06@gmail.com>
2024-11-03 15:42:12 +0900

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './NavBar.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      <header>
+          <div className="navbar">
+              <h1>Hello</h1>
+          </div>
       </header>
+
+      <body>
+        <h1>Welcome to CleanWeb!</h1>
+      </body>
+
+      <footer>
+        <h1></h1>
+      </footer>
     </div>
   );
 }
